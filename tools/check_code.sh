42b981b7747f5d59ad7b8c2b7d831ae87e5ef6b1
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2019-04-14 15:52:11 +0200

    Update check code file with mypy

--- tools/check_code.sh
+++ tools/check_code.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
-python_files=$(git ls-files | grep .py | grep -v tests/)
+PYTHONFILES=$(git ls-files | grep .py | grep -v tests/)
 
-echo ${python_files} | xargs pylint
+echo "Check code script."
+echo "Checking files:\n${PYTHONFILES}\n"
+
+echo "# Pylint tests"
+echo ${PYTHONFILES} | xargs pylint
+
+if [ $? -eq 0 ]; then
+    echo "Passed\n"
+fi
+
+echo "# Mypy tests"
+echo ${PYTHONFILES} | xargs mypy --ignore-missing-imports
+
+if [ $? -eq 0 ]; then
+    echo "Passed"
+fi
