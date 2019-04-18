8c12acec24bb3fa666a770051e07e0728679be44
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2019-04-18 15:19:00 +0200

    Update checker to return failures

--- tools/check_code.sh
+++ tools/check_code.sh
@@ -1,20 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-PYTHONFILES=$(git ls-files | grep .py | grep -v tests/)
+PYTHONFILES=($(git ls-files | grep .py | grep -v tests/ | tr "\n" " "))
 
 echo "Check code script."
-echo "Checking files:\n${PYTHONFILES}\n"
+echo -e "Checking files:\n${PYTHONFILES[@]}\n"
+
+FAILURE=0
 
 echo "# Pylint tests"
-echo ${PYTHONFILES} | xargs pylint
+pylint ${PYTHONFILES[@]}
 
 if [ $? -eq 0 ]; then
-    echo "Passed\n"
+    echo -e "Passed\n"
+else
+    echo -e "Failed\n"
+    FAILURE=1
 fi
 
 echo "# Mypy tests"
-echo ${PYTHONFILES} | xargs mypy --ignore-missing-imports
+mypy --ignore-missing-imports ${PYTHONFILES[@]}
 
 if [ $? -eq 0 ]; then
     echo "Passed"
+else
+    echo "Failed"
+    FAILURE=1
 fi
+
+exit ${FAILURE}
