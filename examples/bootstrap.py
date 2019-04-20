a4e83658d3ecb2fc0a83f6e6461a05ef1e22ad88
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2019-04-20 16:11:24 +0200

    Initial support for bootstrap plots

--- examples/bootstrap.py
+++ examples/bootstrap.py
@@ -7,7 +7,7 @@ from pyroc import ROC, bootstrap_roc
 
 # Simple example to test bootstrap
 ex_rng = np.random.RandomState(37)
-num = 10000
+num = 100
 ex_gt = ex_rng.binomial(1, 0.5, num)
 ex_est = ex_rng.rand((num))
 ex_roc = ROC(ex_gt, ex_est)
@@ -16,7 +16,22 @@ ex_roc_auc_list = [roc.auc for roc in ex_roc_list]
 
 print(f'Average ROC AUC: {np.mean(ex_roc_auc_list)} +/- {np.var(ex_roc_auc_list)**.5}')
 
-ax = plt.gca()
-for roc in ex_roc_list:
-    roc.plot(ax=ax)
+ax = ex_roc.plot(bootstrap=True,
+                 num_bootstraps=1000,
+                 seed=37,
+                 num_bootstrap_jobs=-1,
+                 color='blue',
+                 p_value=0.05,
+                 mean_roc=False,
+                 show_min_max=True)
+ax = ex_roc.plot(bootstrap=True,
+                 num_bootstraps=1000,
+                 seed=37,
+                 num_bootstrap_jobs=-1,
+                 color='red',
+                 p_value=0.35,
+                 mean_roc=True,
+                 show_min_max=False,
+                 ax=ax)
+
 plt.show()
