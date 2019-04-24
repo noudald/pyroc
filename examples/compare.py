e8fd509a97c4b8cb9dc7d2be9f713e2a4e318340
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2019-04-24 10:47:26 +0200

    Added unit tests for compare_bootstrap
    Including several fixes for issues discovered with the unit tests.

--- examples/compare.py
+++ examples/compare.py
@@ -14,7 +14,7 @@ ex_roc1 = ROC(ex_gt, ex_est)
 ex_roc2 = ROC([True, True, True, False, False, False],
               [.9, .8, .35, .4, .3, .1])
 
-print(compare_bootstrap(ex_roc1, ex_roc2))
+print(compare_bootstrap(ex_roc1, ex_roc2, seed=37))
 print(ex_roc1 < ex_roc2)
 print(ex_roc1 <= ex_roc2)
 print(ex_roc1 > ex_roc2)
--- pyroc/compare.py
+++ pyroc/compare.py
@@ -14,13 +14,18 @@ def compare_bootstrap(
         alt_hypothesis: float = 0.05,
         seed: Optional[int] = None) -> Tuple[bool, float]:
     """Compute roc1 < roc2 with alternative hypothesis using bootstrapping."""
+    if not 0 <= alt_hypothesis <= 1:
+        raise ValueError('Alternative hypothesis must be between 0 and 1.')
+
     bootstrap_auc1 = np.array(list(roc.auc
                                    for roc in bootstrap_roc(roc1, seed=seed)))
     bootstrap_auc2 = np.array(list(roc.auc
                                    for roc in bootstrap_roc(roc2, seed=seed)))
 
     aucs = bootstrap_auc2 - bootstrap_auc1
-    sample = np.mean(aucs) / np.std(aucs)
+    sample = np.mean(aucs)
+    if np.std(aucs) > 0:
+        sample /= np.std(aucs)
 
     p_value = 1 - norm.cdf(sample)
 
